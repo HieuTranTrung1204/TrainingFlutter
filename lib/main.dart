a679c300660a07eb4aa3a781a7b927b184dd0297
Puzuka <puzuka.pham@gmail.com>
2018-06-07 14:14:01 +0700

    + http(get/post/put) - chưa xong

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(new MyApp());
+import 'mehod_http.dart';
+void main() => runApp(new MyGetHttpData());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.

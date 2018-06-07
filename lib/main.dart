630531b8cef3f3b734f32cc47e937f4ba54ea733
Puzuka <puzuka.pham@gmail.com>
2018-06-07 16:12:40 +0700

    + update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'flutter_notification_local.dart';
+//import 'flutter_notification_local.dart';
 import 'push_notification_local.dart';
 void main() {
   runApp(

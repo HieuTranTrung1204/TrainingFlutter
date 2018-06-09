987a83776f317a3f1bf004db7de585549cc09991
Puzuka <puzuka.pham@gmail.com>
2018-06-10 05:35:50 +0700

    + tabbar control

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,87 @@
 import 'package:flutter/material.dart';
+import 'tabs/first.dart';
+import 'tabs/second.dart';
+import 'tabs/third.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    // Title
+      title: "Using Tabs",
+      // Home
+      home: new MyHome()));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyHome extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  MyHomeState createState() => new MyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// SingleTickerProviderStateMixin is used for animation
+class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  /*
+   *-------------------- Setup Tabs ------------------*
+   */
+  // Create a tab controller
+  TabController controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the Tab Controller
+    controller = new TabController(length: 3, vsync: this);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+  void dispose() {
+    // Dispose of the Tab Controller
+    controller.dispose();
+    super.dispose();
+  }
+
+  TabBar getTabBar() {
+    return new TabBar(
+      tabs: <Tab>[
+        new Tab(
+          // set icon to the tab
+          icon: new Icon(Icons.favorite),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        new Tab(
+          icon: new Icon(Icons.adb),
+        ),
+        new Tab(
+          icon: new Icon(Icons.airport_shuttle),
+        ),
+      ],
+      // setup the controller
+      controller: controller,
     );
+  }
+
+  TabBarView getTabBarView(var tabs) {
+    return new TabBarView(
+      // Add tabs as widgets
+      children: tabs,
+      // set the controller
+      controller: controller,
+    );
+  }
+
+  /*
+   *-------------------- Setup the page by setting up tabs in the body ------------------*
+   */
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      // Appbar
+        appBar: new AppBar(
+          // Title
+            title: new Text("Using Tabs"),
+            // Set the background color of the App Bar
+            backgroundColor: Colors.blue,
+            // Set the bottom property of the Appbar to include a Tab Bar
+            bottom: getTabBar()),
+        // Set the TabBar view as the body of the Scaffold
+        body: getTabBarView(<Widget>[new First(), new Second(), new Third()]));
   }
 }

a43bcdd7a377f698af74657115dfbadaa25e80bb
TranTrungHieu <hieu.trantrung1204@icloud.com>
2018-06-25 17:45:33 +0700

    + create file sdcard android   Android and iOS: getApplicationDocumentsDirectory();  only Android:  getExternalStorageDirectory();

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,108 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Reading and Writing Files',
+      home: FlutterDemo(storage: CounterStorage()),
+    ),
+  );
+}
 
-void main() => runApp(new MyApp());
+class CounterStorage {
+  Future<String> get _localPath async {
+//    final directory = await getApplicationDocumentsDirectory();
+    final directory = await getExternalStorageDirectory();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(),
-      // Đây là cách set title cách 2
-      //home: new MyHomePage(title: 'Đây là title cách 2'),
-    );
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    // /data/user/0/com.example.sourcedemo/app_flutter
+
+    print("HieuLog: path:" + path);
+//    return File('/storage/sdcard0/Android/data/com.example.sourcedemo/filescounter.txt');
+    return File('$path/counter.png');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If we encounter an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    http.readBytes("https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg").then((bytes){
+      print("HieuLog:" + bytes.toString());
+      bytes.
+      return file.writeAsBytes(bytes);
+    });
+    // Write the file
+    return file.writeAsString('$counter');
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+class FlutterDemo extends StatefulWidget {
+  final CounterStorage storage;
 
-  //Set title cách 2
-  //MyHomePage({Key key, this.title}) : super(key: key);
-  //final String title;
+  FlutterDemo({Key key, @required this.storage}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _FlutterDemoState createState() => _FlutterDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FlutterDemoState extends State<FlutterDemo> {
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _incrementCounter() async {
+    setState(() {
+      _counter++;
+    });
+
+    // write the variable as a string to the file
+    return widget.storage.writeCounter(_counter);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Đây là title cách 1")
-        // title: new Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(title: Text('Reading and Writing Files')),
+      body: Center(
+        child: Text(
+          'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+        ),
       ),
-      body: new Center(
-        child: new Text("Đây là body"),
-      )
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }

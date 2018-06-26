dd83ac499c42bb6dd27db75b1d9ecddeb820e3a7
TranTrungHieu <hieu.trantrung1204@icloud.com>
2018-06-26 16:32:05 +0700

    + sử dụng HttpClient download ảnh

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,84 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:ui' as skyui;
 
-void main() => runApp(new MyApp());
+import 'package:flutter/rendering.dart' as sky;
+import 'package:flutter/services.dart' as sky;
+import 'package:flutter/widgets.dart' as sky;
+import 'package:flutter/material.dart' as sky;
+import 'package:flutter/painting.dart' as sky;
+import 'dart:io' as io;
+import 'dart:typed_data';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  sky.runApp(new MyApp());
+}
+
+class MyApp extends sky.SingleChildRenderObjectWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(),
-      // Đây là cách set title cách 2
-      //home: new MyHomePage(title: 'Đây là title cách 2'),
-    );
+  sky.RenderObject createRenderObject(sky.BuildContext context) {
+    return new DemoObject();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  //Set title cách 2
-  //MyHomePage({Key key, this.title}) : super(key: key);
-  //final String title;
+class DemoObject extends sky.RenderConstrainedBox {
+  double x = 50.0;
+  double y = 50.0;
+  skyui.Image image = null;
+  DemoObject() : super(additionalConstraints: const sky.BoxConstraints.expand()) {
+    ;
+  }
+  loadImage() async {
+    if (image == null) {
+      image = await ImageLoader.load("https://avatars0.githubusercontent.com/u/1310669");
+      this.markNeedsPaint();
+    }
+  }
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  bool hitTestSelf(sky.Offset position) => true;
+
+  @override
+  void handleEvent(sky.PointerEvent event, sky.BoxHitTestEntry entry) {}
+
+  @override
+  void paint(sky.PaintingContext context, sky.Offset offset) {
+    loadImage();
+    sky.Paint paint = new sky.Paint()..color = new sky.Color.fromARGB(255, 255, 0, 0);
+    sky.Offset point = new sky.Offset(x, y);
+    if (image == null) {
+      sky.Rect rect = new sky.Rect.fromLTWH(x, y, 50.0, 50.0);
+      context.canvas.drawRect(rect, paint);
+    } else {
+      context.canvas.drawImage(image, point, paint);
+    }
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Đây là title cách 1")
-        // title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Text("Đây là body"),
-      )
-    );
+class ImageLoader {
+  static Future<skyui.Image> load(String url) async {
+    io.HttpClient client = new io.HttpClient();
+    io.HttpClientRequest request = await client.getUrl(Uri.parse(url));
+    io.HttpClientResponse response = await request.close();
+
+    if (response.statusCode != 200) {
+      throw {"message": "failed to load ${url}"};
+    } else {
+      //Uint8List bytes = new Uint8List(await response.length);
+      int i = 0;
+      List bytesSrc = <int>[];
+      await for (List<int> d in response) {
+        bytesSrc.addAll(d);
+      }
+      Uint8List bytes = new Uint8List.fromList(bytesSrc);
+      // normally use following
+      // import 'package:flutter/services.dart';
+      // Future<ui.Image> decodeImageFromDataPipe(MojoDataPipeConsumer consumerHandle)
+      // Future<ui.Image> decodeImageFromList(Uint8List list) {
+      Completer<skyui.Image> completer = new Completer();
+      skyui.decodeImageFromList(bytes, (skyui.Image image) {
+        completer.complete(image);
+      });
+      return completer.future;
+    }
   }
 }

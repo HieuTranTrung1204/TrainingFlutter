62adf20b5f6da100d44044dc1626b5be5e2aa95c
Trung Hiếu <hieu.trantrung1204@gmail.com>
2019-03-04 15:06:25 +0700

    *+ clear source HelloWorld

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text("Hello world",),
-      ),
-      body: new Text("Body",style: new TextStyle(fontSize: 30.0),) // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+void main() {
+  runApp(Container(
+    color: Colors.white,
+    child: Center(
+      child: Text(
+      "HELLO WORLD", 
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.black, fontSize: 30),),
+    )
+  ));
 }
